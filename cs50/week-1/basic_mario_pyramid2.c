a31ba246c9f52b644b54f734054b24825cb31092
cloneumc <76974750+cloneumc@users.noreply.github.com>
2021-01-06 00:16:57 +0300

    normal mario pyramid
    these are the one way versions of the pyramids of mario we are goint to write a two way version next ;)

---
+++ cs50/week-1/basic_mario_pyramid2.c
@@ -0,0 +1,30 @@
+mario normal pyramid
+
+#include <cs50.h>
+#include <stdio.h>
+int main(void){
+    int height;
+    bool condt = true;
+    do{
+    height = get_int("Height: ");
+    if(height<=0){
+        condt = true;
+    }
+    else if(height>8){
+        condt = true;
+    }
+    else{
+        condt = false;
+        for(int i=0; i<height; i++){
+            for(int k = height; k>i+1; k--){
+                    printf(" ");
+                }
+            for(int j=0; j<i+1; j++){
+                
+            printf("#");
+                }
+            printf("\n");
+        }
+    }}
+while(condt);
+}
