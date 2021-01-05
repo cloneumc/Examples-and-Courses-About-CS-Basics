9594b9fd4f3b168577102174e51285e0d267d923
cloneumc <76974750+cloneumc@users.noreply.github.com>
2021-01-06 00:15:47 +0300

    reversed mario pyramid

---
+++ cs50/week-1/basic_mario_pyramid.c
@@ -0,0 +1,27 @@
+//mario reverse pyramid 
+
+#include <cs50.h>
+#include <stdio.h>
+int main(void){
+    int height;
+    bool condt = true;
+    do{
+    height = get_int("Height: ");
+    if(height<=0){
+        condt = true;
+    }
+    else if(height>8){
+        condt = true;
+    }
+    else{
+        condt = false;
+        for(int i=0; i<height; i++){
+            for(int j=0; j<i+1; j++){
+            printf("#");
+                }
+            printf("\n");
+        }
+    }
+}
+while(condt);
+}
