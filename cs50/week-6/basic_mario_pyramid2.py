5e4f79e1580504af5fbe5aa68228a639bc6f411c
Ufuk Mert <76974750+cloneumc@users.noreply.github.com>
2021-01-07 21:41:16 +0300

    Create basic_mario_pyramid2.py

---
+++ cs50/week-6/basic_mario_pyramid2.py
@@ -0,0 +1,9 @@
+condt=True
+while(condt == True):
+    h = int(input("Height: "))
+    if(h>=1 and h<9):
+        for i in range(h):
+            print(" " * (h - i) + "#" * (i + 1));
+        break
+    else:
+        continue
